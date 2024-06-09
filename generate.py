06806e574e0e0e39c638830bf16f9197f3bbd575
Daniel Fried <dfried@berkeley.edu>
2024-06-09 16:54:19 -0400

    .

--- generate.py
+++ generate.py
@@ -131,7 +131,7 @@ def generate_r_and_p(publications_data):
     latex_output = "\n\n".join(latex_items)
     return latex_output
 
-def generate_html(publications_data, template_file='paper_template.html', group_previous_year=2014, non_preprints_to_include=['conference-papers', 'journal-papers', 'theses', 'workshop-papers']):
+def generate_html(publications_data, template_file='paper-template.html', group_previous_year=2014, non_preprints_to_include=['conference-papers', 'journal-papers', 'theses', 'workshop-papers']):
     data = publications_data
 	# written by ChatGPT
     from jinja2 import Environment, FileSystemLoader
@@ -154,7 +154,7 @@ def generate_html(publications_data, template_file='paper_template.html', group_
 
     # Set up the Jinja2 environment and load the template
     env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template('paper_template.html')
+    template = env.get_template(template_file)
 
     # Render the template with the data
     html_output = template.render(papers_by_year=papers_by_year)
