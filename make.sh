7b762a45cb5f4dde4d7f3dfc982f748b58527a1d
Daniel Fried <dfried@berkeley.edu>
2026-04-28 17:28:35 -0400

    add preprint

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python generate.py cv > cv/publication_list.tex
 python generate.py html > publications-generated.html
+python generate.py group > group/index.html
 
 cd cv/
 pdflatex Fried-CV-web.tex
